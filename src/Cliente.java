4627cdd05d866e2f183d5d98bb1a59edd8e5d916
albernt <albertobernet@gmail.com>
2024-11-12 17:29:36 +0100

    Cambiado aleatoriedad de productos por cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,10 +1,17 @@
+import java.util.Random;
+
 class Cliente {
     private String nombre;
     private Producto[] carrito;
 
-    public Cliente(String nombre, Producto[] carrito) {
+    public Cliente(String nombre, int numProductos) {
         this.nombre = nombre;
-        this.carrito = carrito;
+        this.carrito = new Producto[numProductos];
+        Random random = new Random();
+        for (int i = 0; i < numProductos; i++) {
+            String[] productos = {"Leche", "Pan", "Huevos", "Arepas", "Zanahorias", "Kimchi", "Asedías", "Rabanos", "Pepinos", "Aceitunas", "Tomates", "Colacao", "Alubias", "Vinagre", "Sal"};
+            this.carrito[i] = new Producto(productos[random.nextInt(productos.length)]);
+        }
     }
 
     public String getNombre() {
