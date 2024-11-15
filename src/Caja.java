e32c9e7d52e20d7366b4f0cf40de1a1608230e84
albernt <albertobernet@gmail.com>
2024-11-15 19:33:15 +0100

    Arreglos en la lógica

--- src/Caja.java
+++ src/Caja.java
@@ -1,34 +1,35 @@
-import javax.tools.StandardLocation;
 import java.util.concurrent.TimeUnit;
 
-public class Caja {
+class Caja implements Runnable {
     private int numero;
-    private Cliente clienteActual;
+    private Cliente cliente;
 
     public Caja(int numero) {
         this.numero = numero;
-
     }
 
-    public void cobrar(Cliente cliente) {
-        clienteActual = cliente;
-        System.out.println("El cliente " + clienteActual.getNombre() + " ha sido atendido en la caja " + this.numero);
-        for(Producto producto : clienteActual.getCarrito()) {
-            System.out.println("El cliente " + clienteActual.getNombre() + " compra: " + producto.getNombre());
-            try{
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void run() {
+        if (cliente == null) {
+            System.out.println("Caja " + numero + " no tiene cliente asignado.");
+            return;
+        }
+        System.out.println("El cliente " + cliente.getNombre() + " ha sido atendido en la caja " + this.numero);
+        long initialTime = System.currentTimeMillis();
+        for (Producto producto : cliente.getCarrito()) {
+            System.out.println("El cliente " + cliente.getNombre() + " compra: " + producto.getNombre());
+            try {
                 TimeUnit.MILLISECONDS.sleep(1000);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-            System.out.println("Caja " + numero + ": Producto " + producto.getNombre() + " procesado en " + (System.currentTimeMillis() - this.initialTime) + " ms");
+            System.out.println("Caja " + numero + ": Producto " + producto.getNombre() + " procesado en "
+                    + (System.currentTimeMillis() - initialTime) + " ms");
         }
         System.out.println("Caja " + numero + ": Cliente " + cliente.getNombre() + " procesado");
     }
-
-    private long initialTime = System.currentTimeMillis();
-
-
-
-
-
 }
